9695977f561c954914ba76868a7255ef6773121d
iamadipurohit <iamadipurohit@gmail.com>
2023-06-13 16:15:29 +0530

    not so final

--- client/main.js
+++ client/main.js
@@ -67,7 +67,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   //data lele bot se
-  const response = await fetch("http://localhost:5000", {
+  const response = await fetch("https://ask-ai-ni88.onrender.com", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
